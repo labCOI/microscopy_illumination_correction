919c23d48fab69782575e53f12c7e34be23b3a31
Sama Nemati <88078009+SamaNematy@users.noreply.github.com>
2024-07-23 09:23:48 +0330

    Update mainFunction.m

--- Codes/CIDRE/mainFunction.m
+++ Codes/CIDRE/mainFunction.m
@@ -2,15 +2,14 @@ close all; clear; clc;
 dataset = '49-01';
 tic
 
-source_path = ['C:\Users\Sama\Desktop\Master\Datasets\Empty-Zero\' dataset '/'];
+source_path = ['C:\Users\Sama\Desktop\Master\Datasets\' dataset '/'];
 source_files = dir([source_path '*.jpg']);
-source_files = source_files(81:90);
 
 path_r = ['C:\Users\Sama\Desktop\Master\mfiles\Master Thesis\CIDRE results\' dataset '\s_r\'];
 path_g = ['C:\Users\Sama\Desktop\Master\mfiles\Master Thesis\CIDRE results\' dataset '\s_g\'];
 path_b = ['C:\Users\Sama\Desktop\Master\mfiles\Master Thesis\CIDRE results\' dataset '\s_b\'];
 
-for i = 1:10%length(source_files)
+for i = 1:length(source_files)
     img = imread([source_path source_files(i).name]);
     imwrite(img(:, :, 1), [path_r 'CIDREimg' source_files(i).name]);
     imwrite(img(:, :, 2), [path_g 'CIDREimg' source_files(i).name]);
@@ -42,14 +41,34 @@ I_G_Corr_files = dir([I_G_Corr_dir '*.jpg']);
 I_B_Corr_dir = ['C:\Users\Sama\Desktop\Master\mfiles\Master Thesis\CIDRE results\' dataset '\d_b/'];
 I_B_Corr_files = dir([I_B_Corr_dir '*.jpg']);
 
-path = ['C:\Users\Sama\Desktop\Master\mfiles\Master Thesis\CIDRE results\' dataset '\'];
+path_to_save = ['C:\Users\Sama\Desktop\Master\mfiles\Master Thesis\CIDRE results\' dataset '\'];
 for i = 1:length(I_R_Corr_files)
     I_R = imread([I_R_Corr_dir I_R_Corr_files(i).name]);
     I_G = imread([I_G_Corr_dir I_G_Corr_files(i).name]);
     I_B = imread([I_B_Corr_dir I_B_Corr_files(i).name]);
     
     I_RGB = cat(3,I_R,I_G,I_B); 
-    imwrite(I_RGB, [path I_R_Corr_files(i).name]);
+    imwrite(I_RGB, [path_to_save I_R_Corr_files(i).name]);
 end
 
 toc
+
+%%% visualize_flatfield %%%
+model_r = load(['C:\Users\Sama\Desktop\Master\mfiles\Master Thesis\CIDRE results\' dataset '\d_r\cidre_model.mat']);
+f_r = model_r.model.v;
+f_r = f_r ./ mean(f_r(:));
+
+model_g = load(['C:\Users\Sama\Desktop\Master\mfiles\Master Thesis\CIDRE results\' dataset '\d_g\cidre_model.mat']);
+f_g = model_g.model.v;
+f_g = f_g ./ mean(f_g(:));
+
+model_b = load(['C:\Users\Sama\Desktop\Master\mfiles\Master Thesis\CIDRE results\' dataset '\d_b\cidre_model.mat']);
+f_b = model_b.model.v;
+f_b = f_b ./ mean(f_b(:));
+
+totalRGB_flatfield = cat(3, f_r, f_g, f_b);
+
+totalRGB_flatfield_n = (totalRGB_flatfield - min(totalRGB_flatfield(:))) ./ (max(totalRGB_flatfield(:)) - min(totalRGB_flatfield(:)));
+
+%figure, imshow(totalRGB_flatfield_n); %colorbar;
+imwrite(totalRGB_flatfield_n, [path_to_save 'CIDRE_flatfield_RGBminmaxscale.jpg'])
